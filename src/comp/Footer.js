5817aecc379c19ce3d114214c6d46ff7570020c5
Hussein Elged <elged194@gmail.com>
2024-03-15 15:56:51 -0700

    css

--- src/comp/Footer.js
+++ src/comp/Footer.js
@@ -1,34 +1,31 @@
 import { useTranslation } from "react-i18next";
 
 const Footer = () => {
-  const { t, i18n } = useTranslation();
+  const { t,i18n } = useTranslation();
 
-  if (i18n.language === "ar") {
-    return (
-      <footer dir="rtl">
-        تم التصميم والبرمجه بواسطه حسين الجد elged194@gmail.com
-        <span><i class="fa-brands fa-square-github"></i></span>
-      </footer>
-    );
-  }
+  // if (i18n.language === "ar") {
+  //   return (
+  //     <footer dir="rtl">
+  //       تم التصميم والبرمجه بواسطه حسين الجد elged194@gmail.com
+  //       <span><i class="fa-solid fa-heart"></i></span>
+  //     </footer>
+  //   );
+  // }
 
   if (i18n.language === "en") {
     return (
       <footer >
-        designed and developed by elged194@gmail.com
-        <span><i class="fa-brands fa-square-github"></i></span>
+        <a href="https://github.com/elged194" ><i class="fa-brands fa-square-github"></i></a>
+        <i class="fa-brands fa-instagram"></i>
+        <i class="fa-brands fa-facebook"></i>
+        <a href="https://www.linkedin.com/in/hussein-sadiq-elged/"><i class="fa-brands fa-linkedin"></i></a>
+        <hr/>
+        {t("designed and developed by elged194@gmail.com")}
+        <span><i class="fa-solid fa-heart"></i></span>
       </footer>
     );
   }
 
-  if (i18n.language === "fr") {
-    return (
-      <footer >
-        Concu et developpo par elged194@gmail.com
-        <span><i class="fa-brands fa-square-github"></i></span>
-      </footer>
-    );
-  }
 };
 
 export default Footer;
